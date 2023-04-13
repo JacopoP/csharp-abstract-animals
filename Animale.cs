f5b87abec50c56e2d62770688d4937a0459de569
Jac22 <jacopo.prudentino@gmail.com>
2023-04-13 15:28:29 +0200

    Add project files.

---
+++ Animale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal abstract class Animale
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("Zzz");
+        }
+        public abstract void Verso();
+        public abstract void Mangia();
+    }
+}
---
+++ Cane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    internal class Cane : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Bau!");
+        }
+
+        public override void Mangia() 
+        {
+            Console.WriteLine("Il cane mangia carne");
+        }
+    }
+}
